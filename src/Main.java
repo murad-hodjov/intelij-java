a29756d1dd6dafe6f700712d5c6ec9d45f62aeb8
murad-hodjov <murad.hodjov@gmail.com>
2024-06-03 19:27:09 +0300

    initial comit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Queue;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,7 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome! \n");
-        Queue<Integer> queue =
+        //Queue<Integer> queue =
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
